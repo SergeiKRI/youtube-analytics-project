e7584689c3a7788139a348f18c54173892026dfa
SerKRI <SK@main.ru>
2024-02-21 01:01:17 +0400

    add processing error

--- src/video.py
+++ src/video.py
@@ -1,4 +1,6 @@
 import os
+from pprint import pprint
+
 from googleapiclient.discovery import build
 from dotenv import load_dotenv
 from settings import NAME_DIR
@@ -11,29 +13,40 @@ class Video:
 
     def __init__(self, video_id):
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-
         self.video_id = video_id
         self.video_response = Video.get_serves().videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                                               id=video_id
-                                                               ).execute()
+                                                                   id=video_id
+                                                                   ).execute()
+        if len(self.video_response['items']) == 0:
+            self.video_id = None
 
     def __str__(self):
+        if self.title is None:
+            return ''
         return self.title
 
     @property
     def url_video(self):
+        if self.video_id is None:
+            return None
         return "https://youtu.be/" + self.video_id
 
     @property
     def title(self):
+        if self.video_id is None:
+            return None
         return self.video_response['items'][0]['snippet']['title']
 
     @property
     def view_count(self):
+        if self.video_id is None:
+            return None
         return self.video_response['items'][0]['statistics']['viewCount']
 
     @property
     def like_count(self) -> int:
+        if self.video_id is None:
+            return None
         return self.video_response['items'][0]['statistics']['likeCount']
 
     @classmethod
